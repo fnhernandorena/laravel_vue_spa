1f8629bb708837879cf578a00fa55ddc1a1fe676
Francisco Nicolas Hernandorena <fnhernandorena@gmail.com>
2024-04-22 23:38:39 -0300

    dcsdc

--- resources/js/components/Contact.vue
+++ resources/js/components/Contact.vue
@@ -2,7 +2,8 @@
     <div class="container mt-4">
         <div class="row">
             <div class="col text-center">
-               <h1>Componente <span class="badge bg-secondary">CONTACTO</span></h1>
+               <h1><span class="badge bg-secondary">CONTACT</span> Page</h1>
+        <a class="d-flex w-100 justify-content-center bg-info p-3 text-light rounded-pill" href="https://oficial-site-next-app.vercel.app/">Developer site</a>
             </div>
         </div>
     </div>
--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="container mt-4">
         <div class="row">
-            <div class="col text-center">
-                <h1>Componente <span class="badge bg-secondary">HOME</span></h1>
-                <div v-for="blog in blogs" :key="blog.id">
-                    <div>{{ blog.title }}</div>
-                    <div>{{ blog.description }}</div>
-                </div>
+            <div class="col">
+                <h1 class=" text-center"><span class="badge bg-secondary">HOME</span> Page</h1>
+               <div class="d-flex flex-wrap"> <div v-for="blog in blogs" :key="blog.id">
+                    <div class="card col" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ blog.title }}</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">{{ blog.description }}</h6>
+                        </div>
+                    </div>
+                </div></div>
 
             </div>
         </div>
