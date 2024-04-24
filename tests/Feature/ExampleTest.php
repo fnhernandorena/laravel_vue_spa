014f2cd662781616566df899641b2209b92d197c
Francisco Nicolas Hernandorena <fnhernandorena@gmail.com>
2024-04-24 14:48:40 -0300

    test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -16,4 +16,33 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+     public function test_the_home_page_returns_a_successful_response(): void
+    {
+        $response = $this->get('/blogs');
+
+        $response->assertStatus(200);
+    }
+    public function test_the_contact_page_returns_a_successful_response(): void
+   {
+       $response = $this->get('/contact');
+
+       $response->assertStatus(200);
+   }
+   public function test_the_api_returns_a_successful_response(): void
+   {
+
+    $response = $this->get('/api/blog/');
+
+    $response->assertStatus(200);
+    
+    $response->assertHeader('Content-Type', 'application/json');
+    $jsonContent = $response->json();
+    $this->assertIsArray($jsonContent);
+    $this->assertNotEmpty($jsonContent);
+    foreach ($jsonContent as $item) {
+        $this->assertArrayHasKey('id', $item);
+        $this->assertArrayHasKey('title', $item);
+        $this->assertArrayHasKey('description', $item);
+    }
+}
 }
